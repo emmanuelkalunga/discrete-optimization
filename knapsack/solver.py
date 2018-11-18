0c2eba8dbe4bdf24a2acf3ad1b56e629e805e5b0
Emmanuel Kalunga <emmanuel.kalunga@vastech.co.za>
2018-11-18 14:35:51 +0200

    draft

--- knapsack/solver.py
+++ knapsack/solver.py
@@ -7,11 +7,13 @@ from collections import namedtuple
 from operator import attrgetter
 from collections import namedtuple
 from io import BytesIO, StringIO
+import numpy as np 
 
 Item = namedtuple("Item", ['index', 'value', 'weight', 'density'])
-Block = namedtuple("Block", ['value', 'room', 'estimate'])
+Block = namedtuple("Block", ['value', 'room', 'estimate', 'depth', 'is_tail', 'is_valid', 'taken'])
+Node = namedtuple("Node", ['value', 'room', 'estimate', 'depth', 'is_tail', 'is_valid', 'taken'])
 
-class Node:
+class Node_:
     
     def __init__(self, block, items, left=None, right=None, is_tail=False, is_root=False, is_valid=True, max_depth=0, depth=0, taken=None):
         """
@@ -194,6 +196,84 @@ def size(node):
         return 0 
     else: 
         return (size(node.left)+ 1 + size(node.right)) 
+
+def get_position_in_node_list(node, mylist):
+    step = max( [1, round(len(mylist)/4)] )
+    i = np.random.randint(0, len(mylist))
+
+    #get direction
+    if node.estimate > mylist[i].estimate:
+        direction = 1
+    elif node.estimate < mylist[i].estimate:
+        direction = -1
+    else:
+        return i  # if value is equal to mylist[i], inserst value at position i
+    
+    while (1):
+        if direction > 0:
+            i = min( [i+step, len(mylist)-1] )
+            if node.estimate < mylist[i].estimate:
+                if step == 1:
+                    return i
+                i -= step
+                step = max( [1, round(step/2)] ) 
+            elif node.estimate == mylist[i].estimate:
+                return i
+            elif node.estimate > mylist[i].estimate and i == (len(mylist)-1):
+                return i + 1
+        elif direction < 1:
+            i = max( [i-step, 0] )
+            if node.estimate > mylist[i].estimate:
+                if step == 1:
+                    return i + 1
+                i += step
+                step = max( [1, round(step/2)] ) 
+            elif node.estimate == mylist[i].estimate:
+                return i
+            elif node.estimate < mylist[i].estimate and i == 0:
+                return i
+
+def get_position_in_list(value, mylist):
+    step = max( [1, round(len(mylist)/4)] )
+    i = np.random.randint(0, len(mylist))
+
+    #get direction
+    if value > mylist[i]:
+        direction = 1
+    elif value < mylist[i]:
+        direction = -1
+    else:
+        return i  # if value is equal to mylist[i], inserst value at position i
+    
+    while (1):
+        if direction > 0:
+            i = min( [i+step, len(mylist)-1] )
+            if value < mylist[i]:
+                if step == 1:
+                    return i
+                i -= step
+                step = max( [1, round(step/2)] ) 
+            elif value == mylist[i]:
+                return i
+            elif value > mylist[i] and i == (len(mylist)-1):
+                return i + 1
+        elif direction < 1:
+            i = max( [i-step, 0] )
+            if value > mylist[i]:
+                if step == 1:
+                    return i + 1
+                i += step
+                step = max( [1, round(step/2)] ) 
+            elif value == mylist[i]:
+                return i
+            elif value < mylist[i] and i == 0:
+                return i
+# for value in [0, 12, 13, 50, 51, 100, 200, 300, 600, 900, 999, 1000, 5000, 9999]:
+#     t0 = time.time()
+#     testlist = list(range(100000))
+#     idx = get_position_in_list(value, testlist)
+#     t1 = time.time()
+#     print("getting {} took {} seconds ==> position: {}".format(value, t1-t0, idx))
   
 
 def solve_it(input_data):
@@ -212,21 +292,17 @@ def solve_it(input_data):
         line = lines[i]
         parts = line.split()
         items.append(Item(i, int(parts[0]), int(parts[1]), float(parts[0])/float(parts[1]) ))
+    
+    highest_value = 0
+    max_depth = item_count # Depth go from 0 (root) to item_count. with last item added or not added on the last level
 
-    # A Branch and Bound method
+    # A Branch and Bound method. Best first
     value = 0
     weight = 0
     taken = [0]*len(items)
     optimum = 0
 
     # Get the optimal Estimate value
-
-    # Method 1: Just add together the values of all items   
-    # optimal_estimate = 0
-    # K = capacity
-    # for item in items:
-    #     optimal_estimate += item.value
-    #     K -= item.weight
 
     # Method 2: Use linear relaxation on constraint
     # Sort items my density
@@ -237,24 +313,97 @@ def solve_it(input_data):
     item_dataframe.reset_index(drop=True, inplace=True)
     # print(item_dataframe)
     # Add 1 or 1/x to knapsack until Room left is 0
-    optimal_estimate = 0
+    optimistic_estimate = 0
     K = capacity
     for index, item in item_dataframe.iterrows():
         if K <= 0:
             break
         ratio = max([1, item['weight']/K])
-        optimal_estimate += item['value']/ratio
-        K -= item['weight']/ratio 
+        optimistic_estimate += item['value']/ratio
+        K -= item['weight']/ratio
 
-    # print("Optimal room left: {}, optimal value: {}".format(K, optimal_estimate))
-    
-    # Build the bounded tree: Depth first
+    # Build the bounded tree: Best first
     t0 = time.time()
-    root = Node(Block(0,capacity, optimal_estimate), items, is_root=True, max_depth=item_count)
-    root.insert()
+    root = Node(0, capacity, optimistic_estimate, 0, False, True, [0]*item_count)  # namedtuple("Block", ['value', 'room', 'estimate', 'depth', 'is_tail', 'is_valid', 'taken']) 
+    tails_ordered_list = list()  #Contains node that have been marked as tail and valid
+    nodes_ordered_list = list() #Active nodes: should not contains leaves (any node that has been marked as tail)
+    nodes_ordered_list.insert(root, 0)
+    tails_ordered_list
+
+    # def spawn_left_child(self):
+    #     new_value = self.block.value + self.items[self.depth].value
+    #     # print("Item {} in {}: {} => {}".format(self.depth, len(self.items),self.items[self.depth], new_value))
+    #     new_room = self.block.room - self.items[self.depth].weight
+    #     new_validity = new_room >= 0
+    #     new_estimate = self.block.estimate
+    #     new_depth = self.depth + 1  
+    #     new_taken = (self.taken).copy()
+    #     new_taken[self.depth] = 1
+    #     # print("At level {}, Left child receives {}, and setting element {} to 1 => {}".format(self.depth, self.taken, self.depth, new_taken))
+
+    #     highest_value = max( [self.highest_value, new_value] ) if new_validity else self.highest_value
+
+    #     left_child = Node(block=Block(new_value,new_room,new_estimate), 
+    #                         items=self.items, 
+    #                         is_tail=(new_depth == self.max_depth) or (new_room < 0) or (new_value==new_estimate), 
+    #                         is_valid= new_validity ,
+    #                         max_depth=self.max_depth,
+    #                         depth=new_depth,
+    #                         taken = new_taken
+    #                     )
+    #     left_child.__set_highest_value__(highest_value)
+    #     return left_child
+
+    def split_node(node):
+        left_value = node.value + items[node.depth].value
+        left_room = node.room - items[node.depth].weight
+        left_validity = left_room >= 0 and 
+        left_estimate = node.estimate
+        left_depth = node.depth + 1
+        left_is_tail = (left_depth == max_depth) or (left_room < 0 ) or (left_value==left_estimate)
+        left_taken = (node.taken).copy()
+        left_taken[node.depth] = 1
+        left_child = Node(left_value, left_room, left_estimate, left_depth, left_is_tail, left_validity, left_taken) # namedtuple("Node", ['value', 'room', 'estimate', 'depth', 'is_tail', 'is_valid', 'taken'])
+
+        return (left_child, right_child)
+
+
+    def branchout(nodes_list, tail_list, best_solution_node):
+        if len(nodes_list) > 0:
+            left_child, right_child = split_node(nodes_list[-1])
+            if left_child.is_tail:
+                # Add to tail list (not in active list), and remove parent from active list
+                parent = nodes_list.pop(-1)
+                if left_child.is_valid:
+                    tail_list.append(left_child)
+                    if left_child.value > best_solution_node.value:
+                        best_solution_node = left_child
+            else:
+                # Add to active node list. Replacing parent
+                nodes_list[-1] = left_child  # left child replaces parent
+
+            if right_child.is_tail:
+                # add to list of tails if the tail is valid
+                if right_child.is_valid:
+                    tail_list.append(right_child)
+                    if right_child.value > best_solution_node.value:
+                        best_solution_node = right_child
+            else:
+                # Only insert into list of active nodes if child is not a tail
+                nodes_list.inserst(get_position_in_node_list(right_child, nodes_list), right_child)
+            branchout(nodes_list, tail_list, best_solution_node)
+            
+
+    while(len(nodes_ordered_list)>0):
+        right_child, left_child = branchout_node(nodes_ordered_list[-1])
+    
+
+    
+    
     print("Tree size: {}".format(size(root)))
     # root.print_tree()
-    selected_tails = []
+    selected_tails = list()
+    
     root.get_valid_tails(valid_tails=selected_tails)
     print("There {} valid tails:".format(len(selected_tails)))
     # Print solution and select the best one
